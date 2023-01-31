0a98168b2a8bea8706e61e0c6cdb1e9e525668ad
DESKTOP-AIT5EDG\User <shkrobut.1c@gmail.com>
2023-01-31 16:58:24 +0200

    add file

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ def pretty_print(mas):
     for row in mas:
         print(*row)
     print('-' * 10)
+                    #j
+    # [1,  2,  3,  4],
+    # [5,  6,  7,  8],
+    # [9,  10, 11, 12],
+    # [13, 14, 15, 16],
+   #i   i * 4 + j + 1   9
+   #    2 * 4 + j + 1 = 9
+
+def get_number_from_index(i,j):
+    return i * 4 + j + 1
+
+def get_empty_list(mas):
+    empty = []
+    for i in range(4):
+        for j in range(4):
+            if mas[i][j] == 0:
+                num = get_number_from_index(i,j)
+                empty.append(num)
+    return empty
 
 mas = [
     [0, 0, 0, 0],
@@ -13,5 +32,6 @@ mas = [
 
 mas[1] [2] = 2
 mas[3] [0] = 4
+print(get_empty_list(mas))
 pretty_print(mas)
 
